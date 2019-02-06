d99e97bf2cc94adf4b0bfc44d71ea9abb52c261a
Carsten Behring <carsten.behring@gmail.com>
2019-02-06 22:15:42 +0100

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pppmap "0.1.0-SNAPSHOT"
+(defproject pppmap "0.1.0"
   :description "Partitioned parallel maps with progress"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
